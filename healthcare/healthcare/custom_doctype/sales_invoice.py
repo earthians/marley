1d9cb306f96a819677b2bf44d888df4434d5a6a4
muhammad <muhammad@earthianslive.com>
2020-11-09 11:35:18 +0530

    fix: Healthcare Insurance - set sales invoice item lines

--- healthcare/healthcare/custom_doctype/sales_invoice.py
+++ healthcare/healthcare/custom_doctype/sales_invoice.py
@@ -4,6 +4,14 @@ from erpnext.accounts.doctype.sales_invoice.sales_invoice import SalesInvoice
 
 
 class HealthcareSalesInvoice(SalesInvoice):
+	def validate(self):
+		super(SalesInvoice, self).validate()
+		domain_settings = frappe.get_doc("Domain Settings")
+		active_domains = [d.domain for d in domain_settings.active_domains]
+
+		if "Healthcare" in active_domains:
+			self.calculate_healthcare_insurance_claim()
+
 	@frappe.whitelist()
 	def set_healthcare_services(self, checked_values):
 		from erpnext.stock.get_item_details import get_item_details
@@ -32,7 +40,7 @@ class HealthcareSalesInvoice(SalesInvoice):
 				item_line.rate = checked_item["rate"]
 			else:
 				item_line.rate = item_details.price_list_rate
-			item_line.amount = float(item_line.rate) * float(item_line.qty)
+			# item_line.amount = float(item_line.rate) * float(item_line.qty)
 			if checked_item["income_account"]:
 				item_line.income_account = checked_item["income_account"]
 			if checked_item["dt"]:
@@ -41,10 +49,30 @@ class HealthcareSalesInvoice(SalesInvoice):
 				item_line.reference_dn = checked_item["dn"]
 			if checked_item["description"]:
 				item_line.description = checked_item["description"]
-			if checked_item["dt"] == "Lab Test":
-				lab_test = frappe.get_doc("Lab Test", checked_item["dn"])
-				item_line.service_unit = lab_test.service_unit
-				item_line.practitioner = lab_test.practitioner
-				item_line.medical_department = lab_test.department
-
+			if checked_item["discount_percentage"]:
+				item_line.discount_percentage = checked_item["discount_percentage"]
+			if checked_item["insurance_claim_coverage"]:
+				item_line.insurance_claim_coverage = checked_item["insurance_claim_coverage"]
+			if checked_item["insurance_claim"]:
+				item_line.insurance_claim = checked_item["insurance_claim"]
+			if item_line.discount_percentage and float(item_line.discount_percentage) > 0:
+				item_line.discount_amount = float(item_line.rate) * float(item_line.discount_percentage) * 0.01
+				item_line.rate = float(item_line.rate) - float(item_line.discount_amount)
+			item_line.amount = float(item_line.rate) * float(item_line.qty)
+			if item_line.insurance_claim_coverage and float(item_line.insurance_claim_coverage) > 0:
+				item_line.insurance_claim_amount = (
+					float(item_line.amount) * 0.01 * float(item_line.insurance_claim_coverage)
+				)
+		self.calculate_healthcare_insurance_claim()
 		self.set_missing_values(for_validate=True)
+
+	def calculate_healthcare_insurance_claim(self):
+		total_claim_amount = 0
+		for item in self.items:
+			if item.amount and item.insurance_claim_coverage and float(item.insurance_claim_coverage) > 0:
+				item.insurance_claim_amount = item.amount * 0.01 * float(item.insurance_claim_coverage)
+			if item.insurance_claim_amount and float(item.insurance_claim_amount) > 0:
+				total_claim_amount += float(item.insurance_claim_amount)
+		self.total_insurance_claim_amount = total_claim_amount
+		if self.total_insurance_claim_amount and self.outstanding_amount:
+			self.patient_payable_amount = self.outstanding_amount - self.total_insurance_claim_amount
