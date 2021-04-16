69b0638e50274c9a07dddc369047204567aaf1e0
Rucha Mahabal <ruchamahabal2@gmail.com>
2021-04-16 11:45:24 +0530

    feat: Medications, Allergy and Drug Interactions
    Co-authored-by: Info Aakvatech <info@aakvatech.com>
    Co-authored-by: Anoop Kurungadam <anoop@earthianslive.com>
    Co-authored-by: muhammad <muhammad@earthianslive.com>

--- healthcare/healthcare/doctype/medication/medication.js
+++ healthcare/healthcare/doctype/medication/medication.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Medication', {
+frappe.ui.form.on("Medication", {
 	generic_name: function(frm) {
 		frm.set_value("abbr", frappe.get_abbr(frm.doc.generic_name))
 	},
@@ -51,7 +51,7 @@ frappe.ui.form.on('Medication', {
 	}
 });
 
-frappe.ui.form.on('Medication Linked Item', {
+frappe.ui.form.on("Medication Linked Item", {
 	rate: function(frm, cdt, cdn) {
 		mark_change_in_item(frm, cdt, cdn);
 	},
@@ -71,18 +71,18 @@ frappe.ui.form.on('Medication Linked Item', {
 
 let mark_change_in_item = function(frm, cdt, cdn) {
 	if (!frm.doc.__islocal) {
-		frappe.model.set_value(cdt, cdn, 'change_in_item', 1);
+		frappe.model.set_value(cdt, cdn, "change_in_item", 1);
 	}
 };
 
 let change_medication_code = function(doc) {
 	let d = new frappe.ui.Dialog({
-		title: __('Change Item Code'),
+		title: __("Change Item Code"),
 		fields: [
 			{
-				'fieldtype': 'Data',
-				'label': 'Item Code',
-				'fieldname': 'item_code',
+				"fieldtype": "Data",
+				"label": "Item Code",
+				"fieldname": "item_code",
 				reqd: 1
 			}
 		],
@@ -91,24 +91,24 @@ let change_medication_code = function(doc) {
 
 			if (values) {
 				frappe.call({
-					'method': 'healthcare.healthcare.doctype.medication.medication.change_item_code_from_medication',
-					'args': {item_code: values.item_code, doc: doc},
+					"method": "healthcare.healthcare.doctype.medication.medication.change_item_code_from_medication",
+					"args": {item_code: values.item_code, doc: doc},
 					callback: function () {
 						frm.reload_doc();
 						frappe.show_alert({
-							message: 'Item Code renamed successfully',
-							indicator: 'green'
+							message: "Item Code renamed successfully",
+							indicator: "green"
 						});
 					}
 				});
 			}
 			d.hide();
 		},
-		primary_action_label: __('Change Item Code')
+		primary_action_label: __("Change Item Code")
 	});
 	d.show();
 
 	d.set_values({
-		'item_code': doc.item_code
+		"item_code": doc.item_code
 	});
 };
