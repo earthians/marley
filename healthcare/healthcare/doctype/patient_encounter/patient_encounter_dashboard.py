c202effed6fe334b416bea99e76f97f153a194d3
Rucha Mahabal <ruchamahabal2@gmail.com>
2021-04-29 13:21:00 +0530

    refactor: HSO creation from encounter

--- healthcare/healthcare/doctype/patient_encounter/patient_encounter_dashboard.py
+++ healthcare/healthcare/doctype/patient_encounter/patient_encounter_dashboard.py
@@ -3,15 +3,22 @@ from frappe import _
 
 def get_data():
 	return {
-		"fieldname": "encounter",
-		"non_standard_fieldnames": {
-			"Patient Medical Record": "reference_name",
-			"Inpatient Medication Order": "patient_encounter",
-			"Nursing Task": "reference_name",
+		'fieldname': 'encounter',
+		'non_standard_fieldnames': {
+			'Patient Medical Record': 'reference_name',
+			'Inpatient Medication Order': 'patient_encounter',
+			'Nursing Task': 'reference_name',
+			'Healthcare Service Order': 'order_group'
 		},
-		"transactions": [
-			{"label": _("Records"), "items": ["Vital Signs", "Patient Medical Record"]},
-			{"label": _("Orders"), "items": ["Inpatient Medication Order", "Nursing Task"]},
+		'transactions': [
+			{
+				'label': _('Records'),
+				'items': ['Vital Signs', 'Patient Medical Record']
+			},
+			{
+				'label': _('Orders'),
+				'items': ['Inpatient Medication Order', 'Nursing Task', 'Healthcare Service Order']
+			},
 		],
 		"disable_create_buttons": ["Inpatient Medication Order"],
 	}
