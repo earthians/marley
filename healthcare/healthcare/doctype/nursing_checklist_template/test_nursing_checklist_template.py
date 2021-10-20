2e28783f1b41feef43bed81c6d87a9a40fcfe201
ChillarAnand <anand21nanda@gmail.com>
2021-10-20 16:37:12 +0530

    feat: Added models for nursing tasks

---
+++ healthcare/healthcare/doctype/nursing_checklist_template/test_nursing_checklist_template.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, healthcare and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestNursingChecklistTemplate(unittest.TestCase):
+	pass
---
+++ healthcare/healthcare/doctype/nursing_task/test_nursing_task.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, healthcare and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestNursingTask(unittest.TestCase):
+	pass
