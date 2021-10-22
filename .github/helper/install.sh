856407e90743d44a0e48b6383b07b0a3ecf60b19
ChillarAnand <anand21nanda@gmail.com>
2021-10-22 10:56:08 +0530

    fix(CI): Set erpnext app to frappe repo

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -46,7 +46,7 @@ sed -i 's/schedule:/# schedule:/g' Procfile
 sed -i 's/socketio:/# socketio:/g' Procfile
 sed -i 's/redis_socketio:/# redis_socketio:/g' Procfile
 
-bench get-app https://github.com/chillarAnand/erpnext --branch care
+bench get-app https://github.com/frappe/erpnext
 
 bench start &> bench_run_logs.txt &
 bench --site test_site reinstall --yes
