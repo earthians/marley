bee7bf9b3214cdf8d78af674b420ad5f598bd506
ChillarAnand <anand21nanda@gmail.com>
2021-10-28 10:16:30 +0530

    feat: Added child table for nursing tasks

---
+++ healthcare/healthcare/doctype/healthcare_activity/healthcare_activity.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, healthcare and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Healthcare Activity', {
+	// refresh: function(frm) {
+
+	// }
+});
