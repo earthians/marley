c11fd68bd5f132a985cb1b990e6fcf669b340b69
Anoop Kurungadam <anoop@earthianslive.com>
2022-01-18 16:45:44 +0530

    fix: nursing tasks tests

--- healthcare/healthcare/doctype/nursing_task/test_nursing_task.py
+++ healthcare/healthcare/doctype/nursing_task/test_nursing_task.py
@@ -11,31 +11,29 @@ class TestNursingTask(ERPNextTestCase):
 	def test_creating_nursing_task_from_template(self):
 		task_count = frappe.db.count('Nursing Task')
 
-		template = frappe.get_doc({
-			'doctype': 'Nursing Checklist Template',
-			'title': 'Lab Checklist Template',
-		})
-		template.insert()
+		if not frappe.db.exists('Healthcare Activity', 'BP check'):
+			activity = frappe.get_doc({
+				'doctype': 'Healthcare Activity',
+				'activity': 'BP check',
+			})
+			activity.insert()
 
-		activity = frappe.get_doc({
-			'doctype': 'Healthcare Activity',
-			'activity': 'BP check',
-		})
-		activity.insert()
-
-		task = frappe.get_doc({
-			'doctype': 'Nursing Checklist Template Task',
-			'activity': activity.name,
-			'parent': template,
-			'parenttype': 'Nursing Checklist Template',
-		})
-		task.insert()
-
-		template.append('tasks', task)
-		template.save()
+		template_name = 'Lab Checklist Template'
+		if not frappe.db.exists('Healthcare Activity', template_name):
+			template = frappe.get_doc({
+				'doctype': 'Nursing Checklist Template',
+				'title': template_name,
+			})
+			task = frappe._dict({
+				'doctype': 'Nursing Checklist Template Task',
+				'activity': activity.name,
+				'activity_duration': 900
+			})
+			template.append('tasks', task)
+			template.insert()
 
 		self.lab_template = create_lab_test_template()
-		self.lab_template.nursing_checklist_template = template.name
+		self.lab_template.nursing_checklist_template = template_name
 		self.lab_template.save()
 		create_lab_test(self.lab_template)
 
