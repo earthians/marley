110e20a809ac98d2c45ad10cefdc55498a79cb7e
Chillar Anand <anand21nanda@gmail.com>
2022-02-22 15:45:49 +0530

    feat: Add connection to inpatient record from service unit (#82)

---
+++ healthcare/healthcare/doctype/healthcare_service_unit/healthcare_service_unit_dashboard.py
@@ -0,0 +1,12 @@
+def get_data():
+	return {
+		'fieldname': 'service_unit',
+		'internal_links': {
+			'Inpatient Occupancy': ['inpatient_occupancies']
+		},
+		'transactions': [
+			{
+				'items': ['Inpatient Record']
+			}
+		]
+	}
