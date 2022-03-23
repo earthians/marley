4aff69a62887481de9ef204224b97df9e40c6f79
Anoop Kurungadam <anoop@earthianslive.com>
2022-03-23 15:16:11 +0530

    fix: module import path

--- healthcare/patches/v0_0/setup_service_request_and_insurance.py
+++ healthcare/patches/v0_0/setup_service_request_and_insurance.py
@@ -4,7 +4,7 @@ import frappe
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
 from healthcare.healthcare.healthcare import data
-from healthcare.healthcare.setup import create_customer_groups, setup_service_request_masters
+from healthcare.setup import create_customer_groups, setup_service_request_masters
 
 
 def execute():
