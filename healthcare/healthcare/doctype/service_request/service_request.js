a3373c3c1375c925298ba7d4b0a64903958add02
Akash <akash@earthianslive.com>
2022-11-29 11:47:56 +0530

    fix: Service Request - remove insurance related changes

--- healthcare/healthcare/doctype/service_request/service_request.js
+++ healthcare/healthcare/doctype/service_request/service_request.js
@@ -20,12 +20,12 @@ frappe.ui.form.on('Service Request', {
 			};
 		});
 
-		frm.set_query('order_doctype', function() {
-			let service_order_doctypes = ['Medication', 'Therapy Type', 'Lab Test Template',
+		frm.set_query('template_dt', function() {
+			let order_template_doctypes = ['Therapy Type', 'Lab Test Template',
 				'Clinical Procedure Template'];
 			return {
 				filters: {
-					name: ['in', service_order_doctypes]
+					name: ['in', order_template_doctypes]
 				}
 			};
 		});
@@ -50,83 +50,24 @@ frappe.ui.form.on('Service Request', {
 		frm.trigger('setup_create_buttons');
 	},
 
-	setup_status_buttons: function(frm) {
-		if (frm.doc.docstatus === 1) {
-
-			if (frm.doc.status === 'Active') {
-				frm.add_custom_button(__('On Hold'), function() {
-					frm.events.set_status(frm, 'On Hold');
-				}, __('Status'));
-
-				frm.add_custom_button(__('Completed'), function() {
-					frm.events.set_status(frm, 'Completed');
-				}, __('Status'));
-			}
-
-			if (frm.doc.status === 'On Hold') {
-				frm.add_custom_button(__('Active'), function() {
-					frm.events.set_status(frm, 'Active');
-				}, __('Status'));
-
-				frm.add_custom_button(__('Completed'), function() {
-					frm.events.set_status(frm, 'Completed');
-				}, __('Status'));
-			}
-
-		} else if (frm.doc.docstatus === 2) {
-
-			frm.add_custom_button(__('Revoked'), function() {
-				frm.events.set_status(frm, 'Revoked');
-			}, __('Status'));
-
-			frm.add_custom_button(__('Replaced'), function() {
-				frm.events.set_status(frm, 'Replaced');
-			}, __('Status'));
-
-			frm.add_custom_button(__('Entered in Error'), function() {
-				frm.events.set_status(frm, 'Entered in Error');
-			}, __('Status'));
-
-			frm.add_custom_button(__('Unknown'), function() {
-				frm.events.set_status(frm, 'Unknown');
-			}, __('Status'));
-
-		}
-	},
-
-	set_status: function(frm, status) {
-		frappe.call({
-			method: 'healthcare.healthcare.doctype.service_request.service_request.set_service_request_status',
-			async: false,
-			freeze: true,
-			args: {
-				service_request: frm.doc.name,
-				status: status
-			},
-			callback: function(r) {
-				if (!r.exc) frm.reload_doc();
-			}
-		});
-	},
-
 
 	setup_create_buttons: function(frm) {
 		if (frm.doc.docstatus !== 1 || frm.doc.status === 'Completed') return;
 
-		if (frm.doc.order_doctype === 'Clinical Procedure Template') {
+		if (frm.doc.template_dt === 'Clinical Procedure Template') {
 
 			frm.add_custom_button(__('Clinical Procedure'), function() {
 				frm.trigger('make_clinical_procedure');
 			}, __('Create'));
 
 
-		} else if (frm.doc.order_doctype === 'Lab Test Template') {
+		} else if (frm.doc.template_dt === 'Lab Test Template') {
 
 			frm.add_custom_button(__('Lab Test'), function() {
 				frm.trigger('make_lab_test');
 			}, __('Create'));
 
-		} else if (frm.doc.order_doctype === 'Therapy Type') {
+		} else if (frm.doc.template_dt === 'Therapy Type') {
 
 			frm.add_custom_button(__('Therapy Session'), function() {
 				frm.trigger('make_therapy_session');
