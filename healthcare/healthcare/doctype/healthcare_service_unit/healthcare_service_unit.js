90042f0b3b0b1a03a567784aae433e15a6b8d897
Craft <sajin@earthianslive.com>
2023-02-24 16:38:27 +0530

    fix: Address and Contact in Healthcare Service Unit

--- healthcare/healthcare/doctype/healthcare_service_unit/healthcare_service_unit.js
+++ healthcare/healthcare/doctype/healthcare_service_unit/healthcare_service_unit.js
@@ -16,6 +16,15 @@ frappe.ui.form.on('Healthcare Service Unit', {
 		};
 	},
 	refresh: function(frm) {
+		frm.toggle_display(['address_html', 'contact_html'], !frm.is_new());
+
+		if (!frm.is_new()) {
+			frappe.contacts.render_address_and_contact(frm);
+			frappe.dynamic_link = {doc: frm.doc, fieldname: 'name', doctype: 'Healthcare Service Unit'}
+		} else {
+			frappe.contacts.clear_address_and_contact(frm);
+		}
+
 		frm.trigger('set_root_readonly');
 		frm.set_df_property('service_unit_type', 'reqd', 1);
 		frm.add_custom_button(__('Healthcare Service Unit Tree'), function() {
