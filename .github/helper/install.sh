1e801f23ac31ac56d56eb7f720cc7bcd88e28398
Anoop Kurungadam <anoop@earthianslive.com>
2023-03-06 17:33:46 +0530

    ci: prepare for branching and versioning add mergify, codecov workflows add dependabot update install script update develop branch version to 15.0.0-dev

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -8,7 +8,7 @@ sudo apt-get install redis-server libcups2-dev -qq
 
 pip install frappe-bench
 
-git clone https://github.com/frappe/frappe --branch develop --depth 1
+git clone https://github.com/frappe/frappe --branch "$BRANCH_TO_CLONE" --depth 1
 bench init --skip-assets --frappe-path ~/frappe --python "$(which python)" frappe-bench
 
 mkdir ~/frappe-bench/sites/test_site
@@ -18,7 +18,6 @@ mysql --host 127.0.0.1 --port 3306 -u root -e "SET GLOBAL character_set_server =
 mysql --host 127.0.0.1 --port 3306 -u root -e "SET GLOBAL collation_server = 'utf8mb4_unicode_ci'"
 
 mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE DATABASE test_frappe"
-
 mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE USER 'test_frappe'@'localhost' IDENTIFIED BY 'test_frappe'"
 mysql --host 127.0.0.1 --port 3306 -u root -e "GRANT ALL PRIVILEGES ON \`test_frappe\`.* TO 'test_frappe'@'localhost'"
 
@@ -28,22 +27,30 @@ mysql --host 127.0.0.1 --port 3306 -u root -e "GRANT ALL PRIVILEGES ON \`test_fr
 mysql --host 127.0.0.1 --port 3306 -u root -e "UPDATE mysql.user SET Password=PASSWORD('travis') WHERE User='root'"
 mysql --host 127.0.0.1 --port 3306 -u root -e "FLUSH PRIVILEGES"
 
+install_whktml() {
+    wget -O /tmp/wkhtmltox.tar.xz https://github.com/frappe/wkhtmltopdf/raw/master/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
+    tar -xf /tmp/wkhtmltox.tar.xz -C /tmp
+    sudo mv /tmp/wkhtmltox/bin/wkhtmltopdf /usr/local/bin/wkhtmltopdf
+    sudo chmod o+x /usr/local/bin/wkhtmltopdf
+}
+install_whktml &
 
 cd ~/frappe-bench || exit
-
 
 sed -i 's/watch:/# watch:/g' Procfile
 sed -i 's/schedule:/# schedule:/g' Procfile
 sed -i 's/socketio:/# socketio:/g' Procfile
 sed -i 's/redis_socketio:/# redis_socketio:/g' Procfile
 
-bench get-app erpnext --branch develop --resolve-deps
-bench get-app payments --branch develop --resolve-deps
-
+bench get-app payments --resolve-deps
+bench get-app https://github.com/frappe/erpnext --branch "$BRANCH_TO_CLONE" --resolve-deps
 bench setup requirements --dev
 
-bench start &
+
+bench start &> bench_run_logs.txt &
+CI=Yes bench build --app frappe &
 bench --site test_site reinstall --yes
 
 bench get-app healthcare "${GITHUB_WORKSPACE}"
 bench --verbose --site test_site install-app healthcare
+bench setup requirements --dev
