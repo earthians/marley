0844dbb51837f180d9c8bf6e10b79cc9071a9d30
Akash <akash@earthianslive.com>
2023-03-15 19:51:05 +0530

    fix(test): test exercise set from Therapy Type

--- healthcare/healthcare/doctype/therapy_session/test_therapy_session.py
+++ healthcare/healthcare/doctype/therapy_session/test_therapy_session.py
@@ -7,9 +7,19 @@ import frappe
 from frappe.tests.utils import FrappeTestCase
 from frappe.utils import nowdate
 
+from healthcare.healthcare.doctype.therapy_plan.test_therapy_plan import create_therapy_plan
+
 
 class TestTherapySession(FrappeTestCase):
-	pass
+	def test_exercise_set_from_therapy_type(self):
+		plan = create_therapy_plan()
+		session = create_therapy_session(plan.patient, "Basic Rehab", plan.name)
+		if plan.therapy_plan_details:
+			therapy_type = frappe.get_doc("Therapy Type", plan.therapy_plan_details[0].therapy_type)
+			self.assertEqual(
+				session.exercises[0].exercise_type,
+				therapy_type.exercises[0].exercise_type,
+			)
 
 
 def create_therapy_session(patient, therapy_type, therapy_plan, duration=0, start_date=None):
