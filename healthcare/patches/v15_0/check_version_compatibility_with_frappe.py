82ce8eef77094789469abdc7f9db0949ab62e647
Anoop Kurungadam <anoop@earthianslive.com>
2023-03-17 14:05:07 +0530

    fix: broken healthcare service unit tree view

---
+++ healthcare/patches/v15_0/check_version_compatibility_with_frappe.py
@@ -0,0 +1,19 @@
+import click
+import frappe
+
+
+def execute():
+	frappe_v = frappe.get_attr("frappe" + ".__version__")
+	healthcare_v = frappe.get_attr("healthcare" + ".__version__")
+
+	WIKI_URL = "https://github.com/frappe/health/wiki/changes-to-branching-and-versioning"
+
+	if frappe_v.startswith("14") and healthcare_v.startswith("15"):
+		message = f"""
+			The `develop` branch of Frappe Health is no longer compatible with Frappe & ERPNext's `version-14`.
+			Since you are using ERPNext/Frappe `version-14` please switch Frappe Health app's branch to `version-14` and then proceed with the update.\n\t
+			You can switch the branch by following the steps mentioned here: {WIKI_URL}
+		"""
+		click.secho(message, fg="red")
+
+		frappe.throw(message)  # nosemgrep
