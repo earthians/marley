109be70513f66836b10dd6ecbab8749b4851e476
Sajin SR <sajin@earthianslive.com>
2023-04-20 15:55:39 +0530

    fix: Patient Appointment - fee validity message formating

--- healthcare/healthcare/doctype/patient_appointment/patient_appointment.py
+++ healthcare/healthcare/doctype/patient_appointment/patient_appointment.py
@@ -12,7 +12,7 @@ from frappe import _
 from frappe.core.doctype.sms_settings.sms_settings import send_sms
 from frappe.model.document import Document
 from frappe.model.mapper import get_mapped_doc
-from frappe.utils import flt, get_link_to_form, get_time, getdate
+from frappe.utils import flt, format_date, get_link_to_form, get_time, getdate
 
 from healthcare.healthcare.doctype.healthcare_settings.healthcare_settings import (
 	get_income_account,
@@ -194,8 +194,8 @@ class PatientAppointment(Document):
 		fee_validity = manage_fee_validity(self)
 		if fee_validity:
 			frappe.msgprint(
-				_("{0}: {1} has fee validity till {2}").format(
-					self.patient, frappe.bold(self.patient_name), fee_validity.valid_till
+				_("{0} has fee validity till {1}").format(
+					frappe.bold(self.patient_name), format_date(fee_validity.valid_till)
 				)
 			)
 
