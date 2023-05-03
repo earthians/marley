ffe6f76a2a4f5ae4a0b1094420a9a50972c39041
Akash <akash@earthianslive.com>
2023-05-03 17:53:26 +0530

    fix: tranfer inpatient for procedure

--- healthcare/healthcare/doctype/inpatient_record/inpatient_record.js
+++ healthcare/healthcare/doctype/inpatient_record/inpatient_record.js
@@ -10,7 +10,13 @@ frappe.ui.form.on('Inpatient Record', {
 			{fieldname: 'period', columns: 2},
 			{fieldname: 'dosage_form', columns: 2}
 		];
-	},
+
+		frm.set_indicator_formatter("service_unit", (doc) => {
+			if (doc.left == 0){
+			  return "green";
+			}
+		});
+		},
 	refresh: function(frm) {
 		frm.set_query('admission_service_unit_type', function() {
 			return {
@@ -45,9 +51,12 @@ frappe.ui.form.on('Inpatient Record', {
 				frm.add_custom_button(__("Tranfer"), function() {
 					transfer_patient_dialog(frm);
 				});
-				frm.add_custom_button(__("Transfer For Procedure"), function() {
-					transfer_patient_dialog(frm);
-				});
+				if (!frm.doc.inpatient_occupancies.some(
+					e => e.transferred_for_procedure == 1 && e.left != 1)) {
+						frm.add_custom_button(__("Transfer For Procedure"), function() {
+							transfer_for_procedure_dialog(frm);
+						});
+				}
 			} else if (frm.doc.status == 'Admission Scheduled') {
 				frm.add_custom_button(__('Cancel Admission'), function() {
 					cancel_ip_order(frm)
@@ -248,6 +257,117 @@ let transfer_patient_dialog = function(frm) {
 
 	dialog.show();
 
+	let transferred_for_procedure = frm.doc.inpatient_occupancies.some(
+		e => e.transferred_for_procedure == 1 && e.left != 1)
+	let not_left_service_unit = null;
+	let leave_to_service_unit = null;
+	if (transferred_for_procedure) {
+		for (let inpatient_occupancy in frm.doc.inpatient_occupancies) {
+			if (frm.doc.inpatient_occupancies[inpatient_occupancy].transferred_for_procedure == 1
+				&& frm.doc.inpatient_occupancies[inpatient_occupancy].left != 1) {
+				not_left_service_unit = frm.doc.inpatient_occupancies[inpatient_occupancy].service_unit;
+			}
+			if (frm.doc.inpatient_occupancies[inpatient_occupancy].transferred_for_procedure == 0
+				&&frm.doc.inpatient_occupancies[inpatient_occupancy].left != 1) {
+				leave_to_service_unit = frm.doc.inpatient_occupancies[inpatient_occupancy].service_unit;
+			}
+		}
+	} else {
+		for (let inpatient_occupancy in frm.doc.inpatient_occupancies) {
+			if (frm.doc.inpatient_occupancies[inpatient_occupancy].left != 1) {
+				not_left_service_unit = frm.doc.inpatient_occupancies[inpatient_occupancy].service_unit;
+			}
+		}
+	}
+
+	dialog.set_values({
+		'leave_from': not_left_service_unit
+	});
+	if (leave_to_service_unit) {
+		dialog.set_values({
+			'service_unit': leave_to_service_unit
+		});
+	}
+};
+
+
+let transfer_for_procedure_dialog = function(frm) {
+	let dialog = new frappe.ui.Dialog({
+		title: 'Transfer Patient',
+		width: 100,
+		fields: [
+			{fieldtype: 'Link', label: 'Leave From', fieldname: 'leave_from', options: 'Healthcare Service Unit', reqd: 1, read_only:1},
+			{fieldtype: 'Link', label: 'Service Unit Type', fieldname: 'service_unit_type', options: 'Healthcare Service Unit Type'},
+			{fieldtype: 'Link', label: 'Transfer To', fieldname: 'service_unit', options: 'Healthcare Service Unit', reqd: 1},
+			{fieldtype: 'Datetime', label: 'Check In', fieldname: 'check_in', reqd: 1, default: frappe.datetime.now_datetime()}
+		],
+		primary_action_label: __('Transfer'),
+		primary_action : function() {
+			let service_unit = null;
+			let check_in = dialog.get_value('check_in');
+			let leave_from = null;
+			if(dialog.get_value('leave_from')){
+				leave_from = dialog.get_value('leave_from');
+			}
+			if(dialog.get_value('service_unit')){
+				service_unit = dialog.get_value('service_unit');
+			}
+			if(check_in > frappe.datetime.now_datetime()){
+				frappe.msgprint({
+					title: __('Not Allowed'),
+					message: __('Check-in time cannot be greater than the current time'),
+					indicator: 'red'
+				});
+				return;
+			}
+			frappe.call({
+				doc: frm.doc,
+				method: 'transfer',
+				args:{
+					'service_unit': service_unit,
+					'check_in': check_in,
+					'txred': 1
+				},
+				callback: function(data) {
+					if (!data.exc) {
+						frm.reload_doc();
+					}
+				},
+				freeze: true,
+				freeze_message: __('Process Transfer')
+			});
+			frm.refresh_fields();
+			dialog.hide();
+		}
+	});
+
+	dialog.fields_dict['leave_from'].get_query = function(){
+		return {
+			query : 'healthcare.healthcare.doctype.inpatient_record.inpatient_record.get_leave_from',
+			filters: {docname:frm.doc.name}
+		};
+	};
+	dialog.fields_dict['service_unit_type'].get_query = function(){
+		return {
+			filters: {
+				'inpatient_occupancy': 1,
+				'allow_appointments': 0,
+				'is_ot': 1
+			}
+		};
+	};
+	dialog.fields_dict['service_unit'].get_query = function(){
+		return {
+			filters: {
+				'is_group': 0,
+				'service_unit_type': dialog.get_value('service_unit_type'),
+				'occupancy_status' : 'Vacant'
+			}
+		};
+	};
+
+	dialog.show();
+
 	let not_left_service_unit = null;
 	for (let inpatient_occupancy in frm.doc.inpatient_occupancies) {
 		if (frm.doc.inpatient_occupancies[inpatient_occupancy].left != 1) {
@@ -258,6 +378,7 @@ let transfer_patient_dialog = function(frm) {
 		'leave_from': not_left_service_unit
 	});
 };
+
 
 var schedule_discharge = function(frm) {
 	var dialog = new frappe.ui.Dialog ({
@@ -385,7 +506,6 @@ var create_cancel_treatment_counselling = function(frm) {
 				}, "name")
 				.then(r => {
 					let values = r.message;
-					console.log(values.name)
 					if (values.name) {
 						frappe.confirm(`Treatment Counselling already exist<br>
 						Proceed to Cancel?`,
