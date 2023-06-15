cb799594621988d5050770b76c77b9a3ac8d79f8
Akash <akash@earthianslive.com>
2023-06-15 13:19:36 +0530

    fix: Medical Department in Observation Template and Sample Type in Lab Test sample

---
+++ healthcare/healthcare/doctype/sample_type/test_sample_type.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, healthcare and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSampleType(FrappeTestCase):
+	pass
