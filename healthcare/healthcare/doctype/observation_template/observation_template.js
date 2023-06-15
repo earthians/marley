d9bed41f6e4e37eb6dfe5dac7828024a32d7d631
Akash <akash@earthianslive.com>
2023-06-15 08:14:30 +0530

    fix: Observation Template - reference field changes

--- healthcare/healthcare/doctype/observation_template/observation_template.js
+++ healthcare/healthcare/doctype/observation_template/observation_template.js
@@ -2,6 +2,9 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Observation Template", {
+	onload: function(frm) {
+		set_select_field_options(frm);
+	},
 	refresh: function(frm) {
 		frm.set_query("observation_component", function () {
 			return {
@@ -9,6 +12,13 @@ frappe.ui.form.on("Observation Template", {
 					["Observation Template", "has_component", "=", 0],
 					["Observation Template", "name", "!=", frm.doc.name],
 				]
+			};
+		});
+		frm.set_query("method", function () {
+			return {
+				"filters": {
+					"value_set": "Observation Method"
+				}
 			};
 		});
 	},
@@ -27,4 +37,13 @@ var set_observation_reference_range = function(frm) {
 	$.each(frm.doc.observation_reference_range, function(i, value) {
 		frappe.model.set_value(value.doctype, value.name, "permitted_data_type", frm.doc.permitted_data_type)
 	})
+}
+
+var set_select_field_options = function(frm) {
+	if (frm.doc.permitted_data_type == "Select") {
+		var normal_df = frappe.meta.get_docfield("Observation Reference Range","normal_select", cur_frm.doc.name);
+		var abnormal_df = frappe.meta.get_docfield("Observation Reference Range","abnormal_select", cur_frm.doc.name);
+		var critical_df = frappe.meta.get_docfield("Observation Reference Range","critical_select", cur_frm.doc.name);
+		normal_df.options = abnormal_df.options = critical_df.options = frm.doc.options;
+	}
 }
