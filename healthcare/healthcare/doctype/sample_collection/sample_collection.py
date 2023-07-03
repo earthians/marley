afe3e20a34ff7bd5a9d85bd9c0c3aadb518f4998
Akash <akash@earthianslive.com>
2023-07-03 07:16:14 +0530

    fix: Sample Collection - create specimen on collection

--- healthcare/healthcare/doctype/sample_collection/sample_collection.py
+++ healthcare/healthcare/doctype/sample_collection/sample_collection.py
@@ -82,7 +82,8 @@ def create_observation(selected, sample_collection, component_observations=[], c
 	if len(component_observations) > 0:
 		component_observations = json.loads(component_observations)
 	patient = frappe.db.get_value("Sample Collection", sample_collection, "patient")
-	for obs in selected:
+	comp_obs_ref = create_specimen(selected, component_observations)
+	for i, obs in enumerate(selected):
 		if obs.get("status") == "Open":
 			if not obs.get("has_component") or obs.get("has_component") == 0:
 				observation = add_observation(
@@ -93,20 +94,24 @@ def create_observation(selected, sample_collection, component_observations=[], c
 					"Sample Collection",
 					sample_collection,
 					obs.get("component_observation_parent"),
-					obs.get("sample_id"),
+					comp_obs_ref.get(obs.get("name")) or comp_obs_ref.get(i+1) or comp_obs_ref.get(obs.get("idx")),
 					invoice
 				)
 				if observation:
 					frappe.db.set_value(
 						"Observation Sample Collection",
 						obs.get("name"),
-						{"status": "Completed", "collection_date_time": now_datetime()},
+						{
+							"status": "Completed",
+							"collection_date_time": now_datetime(),
+							"specimen": comp_obs_ref.get(obs.get("name")),
+						},
 					)
 			else:
 				# to deal the component template checket from main table and collected
 				if obs.get("component_observations"):
 					component_observations = json.loads(obs.get("component_observations"))
-					for comp in component_observations:
+					for j, comp in enumerate(component_observations):
 						observation = add_observation(
 							patient,
 							comp.get("observation_template"),
@@ -115,30 +120,73 @@ def create_observation(selected, sample_collection, component_observations=[], c
 							"Sample Collection",
 							sample_collection,
 							obs.get("component_observation_parent"),
-							obs.get("sample_id"),
+							comp_obs_ref.get(j+1) or comp_obs_ref.get(obs.get("name")),
 							invoice
 						)
 						if observation:
 							comp["status"] = "Completed"
 							comp["collection_date_time"] = now_datetime()
+							comp["specimen"] = comp_obs_ref.get(j+1) or comp_obs_ref.get(obs.get("name"))
 
 					frappe.db.set_value(
 						"Observation Sample Collection",
 						obs.get("name"),
 						{
+							"collection_date_time": now_datetime(),
 							"component_observations": json.dumps(component_observations, default=str),
-							"status": "Completed"
+							"status": "Completed",
+							"specimen": comp_obs_ref.get(j+1) or comp_obs_ref.get(obs.get("name")),
 						},
 					)
 		# to deal individually checked from component dialog
 		if component_observations:
-			for comp in component_observations:
+			for j, comp in enumerate(component_observations):
 				if comp.get("observation_template") == obs.get("observation_template"):
 					comp["status"] = "Completed"
 					comp["collection_date_time"] = now_datetime()
+					comp["specimen"] = comp_obs_ref.get(j+1)
+
+	child_db_set_dict = {"component_observations": json.dumps(component_observations, default=str)}
+	# to set child table status completed if all childs are completed
+	if not any((comp['status'] == "Open") for comp in component_observations):
+		child_db_set_dict["status"] = "Completed"
+
 	if child_name:
 		frappe.db.set_value(
 			"Observation Sample Collection",
 			child_name,
-			{"component_observations": json.dumps(component_observations, default=str)},
+			child_db_set_dict,
 		)
+
+def create_specimen(selected, component_observations):
+	groups = {}
+	# to group by
+	for sel in selected:
+		if not sel.get("has_component") or sel.get("has_component") == 0:
+			key = (sel.get("color"), sel.get('medical_department'), sel.get('sample_Type'), sel.get("container_closure_color"))
+			if key in groups:
+				groups[key].append(sel)
+			else:
+				groups[key] = [sel]
+		else:
+			comp_observations = json.loads(sel.get("component_observations"))
+			for comp in comp_observations:
+				comp["name"] = sel.get("name")
+				key = (comp.get("color"), comp.get('medical_department'), comp.get('sample_Type'), comp.get("container_closure_color"))
+				if key in groups:
+					groups[key].append(comp)
+				else:
+					groups[key] = [comp]
+
+	obs_ref = {}
+	for gr in groups:
+		specimen = frappe.new_doc("Specimen")
+		specimen.received_time = now_datetime()
+		specimen.save()
+		for sub_grp in groups[gr]:
+			if component_observations:
+				obs_ref[sub_grp.get("idx")] = specimen.name
+			else:
+				obs_ref[sub_grp.get("name")] = specimen.name
+
+	return obs_ref
