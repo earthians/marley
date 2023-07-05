27a167cc98b7f6edc648de512f999b08e2b0b3a8
Akash <akash@earthianslive.com>
2023-07-05 17:13:39 +0530

    fix: Diagnostic Report - widget change

--- healthcare/public/js/healthcare.bundle.js
+++ healthcare/public/js/healthcare.bundle.js
@@ -4,3 +4,4 @@ import "./observation.html";
 import "./diagnostic_report_controller.js";
 import "./observation_widget.js";
 import "./observation_widget.html";
+import "./observation_component_widget.html";
