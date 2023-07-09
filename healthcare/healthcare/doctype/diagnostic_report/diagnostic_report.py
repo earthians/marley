acb6ede6822f0a073a8a470954682c7f2072a630
Anoop Kurungadam <anoop@earthianslive.com>
2023-07-09 15:46:35 +0530

    fix: add patient age to diagnostic report

--- healthcare/healthcare/doctype/diagnostic_report/diagnostic_report.py
+++ healthcare/healthcare/doctype/diagnostic_report/diagnostic_report.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2023, healthcare and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+
+from healthcare.healthcare.doctype.observation.observation import calculate_age
 
 
 class DiagnosticReport(Document):
-	pass
+	def validate(self):
+		self.set_age()
+
+	def set_age(self):
+		if not self.age:
+			dob = frappe.db.get_value("Patient", self.patient, "dob")
+			if dob:
+				self.age = calculate_age(dob)
