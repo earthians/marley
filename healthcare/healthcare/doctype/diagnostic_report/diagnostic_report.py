f2d40cc5abd314f93c653b39551b381c04b11779
Anoop Kurungadam <anoop@earthianslive.com>
2023-07-16 18:50:51 +0530

    fix: set list view fieds, filters set title for Diagnostic Report

--- healthcare/healthcare/doctype/diagnostic_report/diagnostic_report.py
+++ healthcare/healthcare/doctype/diagnostic_report/diagnostic_report.py
@@ -2,27 +2,32 @@
 # For license information, please see license.txt
 
 import frappe
-import json
 from frappe.model.document import Document
-from frappe.utils import format_date
 
-from healthcare.healthcare.doctype.observation.observation import calculate_age, get_observation_details
+from healthcare.healthcare.doctype.observation.observation import (
+	calculate_age,
+	get_observation_details,
+)
 
 
 class DiagnosticReport(Document):
 	def validate(self):
 		self.set_age()
 		self.set_invoice_status()
+		self.set_title()
 
 	def before_insert(self):
 		if self.ref_doctype == "Sales Invoice" and self.docname:
-			self.practitioner =  frappe.db.get_value(self.ref_doctype, self.docname, "ref_practitioner")
+			self.practitioner = frappe.db.get_value(self.ref_doctype, self.docname, "ref_practitioner")
 
 	def set_age(self):
 		if not self.age:
 			dob = frappe.db.get_value("Patient", self.patient, "dob")
 			if dob:
 				self.age = calculate_age(dob)
+
+	def set_title(self):
+		self.title = f"{self.patient_name} - {self.age or ''} {self.gender}"
 
 	def set_invoice_status(self):
 		if self.ref_doctype == "Sales Invoice" and self.docname:
