55e6c2a8cccde518309238437563b0072a5fc5fa
Akash <akash@earthianslive.com>
2023-07-20 07:35:31 +0530

    fix: Imaging in Diagnostic Report Widget

--- healthcare/public/js/diagnostic_report_controller.js
+++ healthcare/public/js/diagnostic_report_controller.js
@@ -39,6 +39,13 @@ healthcare.Diagnostic.Observation = class Observation {
 		$(".observation").find(".add-note-observation-btn").on("click", function() {
 			me.add_note(this)
 		});
+
+		$(".observation").find(".btn-findings").on("click", function() {
+			me.add_finding_interpretation(this, ".btn-findings")
+		});
+		$(".observation").find(".btn-interpr").on("click", function() {
+			me.add_finding_interpretation(this, ".btn-interpr")
+		});
     }
 
 	save_action(func) {
@@ -121,4 +128,90 @@ healthcare.Diagnostic.Observation = class Observation {
 			d.show();
 	}
 
+	add_finding_interpretation (edit_btn, btn_attr) {
+		var me = this;
+		let row = $(edit_btn).closest('.observation');
+		let observation_name = row.attr("name");
+		let template = $(row).find(btn_attr).attr("name")
+		let result_html = $(row).find(btn_attr+"-text").html();
+		var d = new frappe.ui.Dialog({
+			title: btn_attr == ".btn-findings" ? __('Add Findings') : __('Add Interpretation'),
+			fields: [
+				{
+					"label": "Observation",
+					"fieldname": "observation",
+					"fieldtype": "Link",
+					"options": "Observation",
+					"default": observation_name,
+					"hidden": 1,
+				},
+				{
+					"label": "Template",
+					"fieldname": "template",
+					"fieldtype": "Link",
+					"options": "Terms and Conditions",
+					"default": template,
+				},
+				{
+					"label": "Note",
+					"fieldname": "note",
+					"fieldtype": "Text Editor",
+					"default" : result_html,
+					reqd: 1,
+				}
+			],
+			primary_action: function() {
+				var data = d.get_values();
+				let val_dict = {};
+				var values = [];
+				val_dict["observation"] = observation_name
+				val_dict["result"] = ""
+				if (btn_attr == ".btn-findings") {
+					val_dict["result"] = data.note
+				} else if (btn_attr == ".btn-interpr") {
+					val_dict["interpretation"] = data.note
+				}
+				values.push(val_dict);
+				frappe.call({
+					method: "healthcare.healthcare.doctype.observation.observation.record_observation_result",
+					args: {
+						values: values
+					},
+					freeze: true,
+					callback: function(r) {
+						if (!r.exc) {
+							me.refresh();
+						}
+						d.hide();
+					}
+				});
+			},
+			primary_action_label: __("Save")
+			});
+			if ((!result_html || result_html.trim() == "") && d.get_values("template")) {
+				set_text_to_dialog(d)
+			  }
+			d.fields_dict['template'].df.onchange = () => {
+				const regex = /<p>(.*?)<\/p>/;
+				const match = d.get_value("note").match(regex);
+				const result_value = match ? match[1] : null;
+				if (d.get_value('template') && (!result_value || result_value == "<br>")) {
+					set_text_to_dialog(d)
+				}
+			}
+			d.show();
+	}
+}
+
+var set_text_to_dialog = function(d) {
+	frappe.call({
+		method: 'healthcare.healthcare.doctype.observation.observation.get_observation_result_template',
+		args: {
+			template_name: d.get_value("template"),
+			observation: d.get_value("observation")
+		},
+		callback: function (r) {
+			d.set_value('note', r.message)
+		}
+	});
 }
