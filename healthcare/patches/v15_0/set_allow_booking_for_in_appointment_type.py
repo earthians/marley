e661def3bb354e7de435cc4649993cd2123bf8af
Akash <akash@earthianslive.com>
2023-07-25 14:25:58 +0530

    fix: Linters Issue

--- healthcare/patches/v15_0/set_allow_booking_for_in_appointment_type.py
+++ healthcare/patches/v15_0/set_allow_booking_for_in_appointment_type.py
@@ -4,12 +4,8 @@ import frappe
 def execute():
 	appointment_types = frappe.db.get_all("Appointment Type")
 	for at in appointment_types:
-		frappe.db.set_value(
-			"Appointment Type", at.name, "allow_booking_for", "Practitioner"
-		)
+		frappe.db.set_value("Appointment Type", at.name, "allow_booking_for", "Practitioner")
 
 	appointment_type_items = frappe.db.get_all("Appointment Type Service Item")
 	for ati in appointment_type_items:
-		frappe.db.set_value(
-			"Appointment Type Service Item", ati.name, "dt", "Medical Department"
-		)
+		frappe.db.set_value("Appointment Type Service Item", ati.name, "dt", "Medical Department")
