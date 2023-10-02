2e485a9f1ebccc0764307652be33a2783e1c0017
Akash <akash@earthianslive.com>
2023-10-02 10:15:30 +0530

    fix: Diagnostic Report - up, down, enter key input field navigation

--- healthcare/public/js/diagnostic_report_controller.js
+++ healthcare/public/js/diagnostic_report_controller.js
@@ -16,6 +16,33 @@ healthcare.Diagnostic.DiagnosticReport = class DiagnosticReport {
 			freeze: true,
 			callback: function(r) {
 				me.create_widget(r)
+				let new_ob_list = [];
+				const inputContainer = document.querySelectorAll('.input-with-feedback');
+				for (let i = 0; i < inputContainer.length; i++) {
+					new_ob_list.push(inputContainer[i])
+				}
+
+				document.addEventListener('keydown', function(event) {
+					const focusedElement = document.activeElement;
+					let current_index = 0
+					let next_index = 0
+					for (let key in new_ob_list) {
+						if (new_ob_list.hasOwnProperty(key) && new_ob_list.includes(focusedElement)) {
+							current_index = new_ob_list.indexOf(focusedElement);
+						}
+					}
+
+					if (['ArrowDown', 'Enter'].includes(event.key)) {
+						next_index = current_index + 1
+					} else if (event.key === 'ArrowUp') {
+						next_index = current_index - 1
+					}
+
+					if (new_ob_list[next_index]) {
+						new_ob_list[next_index].focus();
+					}
+
+				});
 				}
 			})
 			me.save_action("load")
@@ -51,4 +78,5 @@ healthcare.Diagnostic.DiagnosticReport = class DiagnosticReport {
 			})
 		}
 	}
+
 }
