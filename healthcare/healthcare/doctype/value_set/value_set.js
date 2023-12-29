c6cd88bb5865292350ce7f2d3b41a707cd824831
Akash <akash@earthianslive.com>
2023-12-29 19:01:52 +0530

    fix: Value Set Code Value - fetch code_system from value_set

---
+++ healthcare/healthcare/doctype/value_set/value_set.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, earthians Health Informatics Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Value Set", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ healthcare/healthcare/doctype/value_set/value_set.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, earthians Health Informatics Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ValueSet(Document):
+	pass
