0b13c8db5ef3769d5d987ee6250b6dba157b6355
Akash <akash@earthianslive.com>
2024-02-07 12:55:15 +0530

    test: Service Request - status completed on reference doc submission

--- healthcare/healthcare/doctype/service_request/test_service_request.py
+++ healthcare/healthcare/doctype/service_request/test_service_request.py
@@ -10,6 +10,9 @@ from frappe.utils import getdate, nowtime
 
 import erpnext
 
+from healthcare.healthcare.doctype.clinical_procedure.test_clinical_procedure import (
+	create_procedure,
+)
 from healthcare.healthcare.doctype.healthcare_settings.healthcare_settings import (
 	get_income_account,
 	get_receivable_account,
@@ -22,36 +25,62 @@ from healthcare.healthcare.doctype.observation_template.test_observation_templat
 	create_observation_template,
 )
 from healthcare.healthcare.doctype.patient_appointment.test_patient_appointment import (
+	create_clinical_procedure_template,
 	create_healthcare_docs,
 )
+from healthcare.healthcare.doctype.service_request.service_request import make_clinical_procedure
 
 
 class TestServiceRequest(unittest.TestCase):
 	def test_creation_on_encounter_submission(self):
 		patient, practitioner = create_healthcare_docs()
 		insulin_resistance_template = create_lab_test_template()
+		procedure_template = create_clinical_procedure_template()
+		procedure_template.allow_stock_consumption = 1
 		encounter = create_encounter(
-			patient, practitioner, "lab_test_prescription", insulin_resistance_template, submit=True
+			patient,
+			practitioner,
+			"lab_test_prescription",
+			insulin_resistance_template,
+			procedure_template,
+			submit=True,
 		)
 		self.assertTrue(frappe.db.exists("Service Request", {"order_group": encounter.name}))
-		service_request = frappe.db.get_value("Service Request", {"order_group": encounter.name}, "name")
+		service_request = frappe.db.get_all(
+			"Service Request", {"order_group": encounter.name}, ["name", "template_dt"]
+		)
 		if service_request:
-			service_request_doc = frappe.get_doc("Service Request", service_request)
-			service_request_doc.submit()
-			lab_test = create_lab_test(insulin_resistance_template)
-			lab_test.service_request = service_request
-			lab_test.descriptive_test_items[0].result_value = 1
-			lab_test.descriptive_test_items[1].result_value = 2
-			lab_test.descriptive_test_items[2].result_value = 3
-			lab_test.submit()
-			# create sales invoice with service request and check service request and lab test is marked as invoiced
-			create_sales_invoice(
-				patient, service_request_doc, insulin_resistance_template, "lab_test_prescription"
-			)
-			self.assertEqual(
-				frappe.db.get_value("Service Request", service_request_doc.name, "billing_status"), "Invoiced"
-			)
-			self.assertTrue(frappe.db.get_value("Lab Test", lab_test.name, "invoiced"))
+			for serv in service_request:
+				service_request_doc = frappe.get_doc("Service Request", serv.get("name"))
+				service_request_doc.submit()
+				if serv.get("name"):
+					if serv.get("template_dt") == "Lab Test Template":
+						template = insulin_resistance_template
+						type = "lab_test_prescription"
+						doc = "Lab Test"
+						test = create_lab_test(template)
+						test.service_request = serv.get("name")
+						test.descriptive_test_items[0].result_value = 1
+						test.descriptive_test_items[1].result_value = 2
+						test.descriptive_test_items[2].result_value = 3
+						test.submit()
+					elif serv.get("template_dt") == "Clinical Procedure Template":
+						test = make_clinical_procedure(service_request_doc)
+						test.submit()
+						doc = "Clinical Procedure"
+						template = procedure_template
+						type = "procedure_prescription"
+
+					# create sales invoice with service request and check service request and lab test is marked as invoiced
+					create_sales_invoice(patient, service_request_doc, template, type)
+					self.assertEqual(
+						frappe.db.get_value("Service Request", serv.get("name"), "billing_status"), "Invoiced"
+					)
+					self.assertEqual(
+						frappe.db.get_value("Service Request", serv.get("name"), "status"),
+						"completed-Request Status",
+					)
+					self.assertTrue(frappe.db.get_value(doc, test.name, "invoiced"))
 
 	def test_creation_on_encounter_with_create_order_on_save_checked(self):
 		patient, practitioner = create_healthcare_docs()
@@ -87,7 +116,9 @@ class TestServiceRequest(unittest.TestCase):
 			self.assertEqual(frappe.db.get_value("Observation", observation.name, "invoiced"), 1)
 
 
-def create_encounter(patient, practitioner, type, template, submit=False, obs=False):
+def create_encounter(
+	patient, practitioner, type, template, procedure_template=False, submit=False, obs=False
+):
 	patient_encounter = frappe.new_doc("Patient Encounter")
 	patient_encounter.patient = patient
 	patient_encounter.practitioner = practitioner
@@ -102,10 +133,18 @@ def create_encounter(patient, practitioner, type, template, submit=False, obs=Fa
 			patient_encounter.append(type, {"medication": template.name})
 	else:
 		patient_encounter.append(type, {"observation_template": template.name})
+
+	if procedure_template:
+		patient_encounter.append(
+			"procedure_prescription",
+			{"procedure": procedure_template.template, "procedure_name": procedure_template.item_code},
+		)
+
 	if submit:
 		patient_encounter.submit()
 	else:
 		patient_encounter.save()
+
 	return patient_encounter
 
 
@@ -150,7 +189,7 @@ def create_sales_invoice(patient, service_request, template, type):
 				"description": template.name,
 			},
 		)
-	elif type == "observation":
+	elif type in ["observation", "procedure_prescription"]:
 		sales_invoice.append(
 			"items",
 			{
@@ -168,6 +207,7 @@ def create_sales_invoice(patient, service_request, template, type):
 				"description": template.description,
 			},
 		)
+
 	sales_invoice.set_missing_values()
 
 	sales_invoice.submit()
