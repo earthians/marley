0fc0e5fc85990d53a8ffb1439694ea5428edfe5b
Sajin SR <sajin@earthianslive.com>
2024-07-18 19:32:00 +0530

    refactor: replace Frappe Health references with Marley Health

--- healthcare/patches/v15_0/check_version_compatibility_with_frappe.py
+++ healthcare/patches/v15_0/check_version_compatibility_with_frappe.py
@@ -11,8 +11,8 @@ def execute():
 
 	if frappe_v.startswith("14") and healthcare_v.startswith("15"):
 		message = f"""
-			The `develop` branch of Frappe Health is no longer compatible with Frappe & ERPNext's `version-14`.
-			Since you are using ERPNext/Frappe `version-14` please switch Frappe Health app's branch to `version-14` and then proceed with the update.\n\t
+			The `develop` branch of Marley Health is no longer compatible with Frappe & ERPNext's `version-14`.
+			Since you are using ERPNext/Frappe `version-14` please switch Marley Health app's branch to `version-14` and then proceed with the update.\n\t
 			You can switch the branch by following the steps mentioned here: {WIKI_URL}
 		"""
 		click.secho(message, fg="red")
