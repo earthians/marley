3a736f5221606d226aa0d5d687d7268273db7f89
Sajin SR <sajin@earthianslive.com>
2024-10-22 08:17:37 +0530

    fix(Treatment Counselling): Add payment entry on_cancel and paid amount calculation

--- healthcare/healthcare/custom_doctype/payment_entry.py
+++ healthcare/healthcare/custom_doctype/payment_entry.py
@@ -4,9 +4,19 @@ import frappe
 @frappe.whitelist()
 def set_paid_amount_in_treatment_counselling(doc, method):
 	if doc.treatment_counselling and doc.paid_amount:
-		treatment_counselling_doc = frappe.get_doc("Treatment Counselling", doc.treatment_counselling)
-		treatment_counselling_doc.paid_amount = doc.paid_amount
-		treatment_counselling_doc.outstanding_amount = (
-			treatment_counselling_doc.amount - treatment_counselling_doc.paid_amount
-		)
-		treatment_counselling_doc.save()
+		on_cancel = True if method == "on_cancel" else False
+		validate_treatment_counselling(doc, on_cancel)
+
+
+def validate_treatment_counselling(doc, on_cancel=False):
+	treatment_counselling_doc = frappe.get_doc("Treatment Counselling", doc.treatment_counselling)
+
+	paid_amount = treatment_counselling_doc.paid_amount + doc.paid_amount
+	if on_cancel:
+		paid_amount = treatment_counselling_doc.paid_amount - doc.paid_amount
+
+	treatment_counselling_doc.paid_amount = paid_amount
+	treatment_counselling_doc.outstanding_amount = (
+		treatment_counselling_doc.amount - treatment_counselling_doc.paid_amount
+	)
+	treatment_counselling_doc.save()
