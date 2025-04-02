26703edf73b9519569301fd87ca4383618040af3
Anoop Kurungadam <anoop@earthianslive.com>
2025-04-02 21:29:56 +0530

    fix: status display in Service / Medication Request list

--- healthcare/healthcare/doctype/medication_request/medication_request_list.js
+++ healthcare/healthcare/doctype/medication_request/medication_request_list.js
@@ -3,16 +3,24 @@ frappe.listview_settings[('Medication Request')] = {
 	filters: [['docstatus', '=', '1']],
 	has_indicator_for_cancelled: 1,
 	get_indicator: function (doc) {
-		return [__(doc.status), {
-			'active-Medication Request Status': 'green',
-			'on-hold-Medication Request Status': 'yellow',
-			'stopped-Medication Request Status': 'orange',
-			'ended-Medication Request Status': 'grey',
-			'completed-Medication Request Status': 'blue',
-			'cancelled-Medication Request Status': 'yellow',
-			'entered-in-error-Medication Request Status': 'red',
-			'draft-Medication Request Status': 'orange',
-			'unknown-Medication Request Status': 'grey',
-		}[doc.status], 'status,=,' + doc.status];
+		if (doc.status == "draft-Medication Request Status") {
+			return [__("Draft"), "orange", "status,=,Draft"];
+		} else if (doc.status == "stopped-Medication Request Status") {
+			return [__("Stopped"), "orange", "status,=,Stopped"];
+		} else if (doc.status == "active-Medication Request Status") {
+			return [__("Active"), "green", "status,=,Active"];
+		} else if (doc.status == "on-hold-Medication Request Status") {
+			return [__("On Hold"), "yellow", "status,=,On Hold"];
+		} else if (doc.status == "completed-Medication Request Status") {
+			return [__("Completed"), "blue", "status,=,Completed"];
+		} else if (doc.status == "ended-Medication Request Status") {
+			return [__("Ended"), "grey", "status,=,Ended"];
+		} else if (doc.status == "cancelled-Medication Request Status") {
+			return [__("Cancelled"), "grey", "status,=,Cancelled"];
+		} else if (doc.status == "entered-in-error-Medication Request Status") {
+			return [__("Entered In Error"), "red", "status,=,Entered In Error"];
+		} else { //if (doc.status == "unknown-Medication Request Status") {
+			return [__("Unknown"), "grey", "status,=,Unknown"];
+		}
 	}
 };
