b1f01aaf94ca80ec807f7f5f3eb00491b919ae94
Sajin SR <sajin@earthianslive.com>
2025-05-26 13:03:29 +0530

    refactor: move links to insurance_payor_eligibility_plan_dashboard.py

--- healthcare/healthcare/doctype/insurance_payor_eligibility_plan/insurance_payor_eligibility_plan_dashboard.py
+++ healthcare/healthcare/doctype/insurance_payor_eligibility_plan/insurance_payor_eligibility_plan_dashboard.py
@@ -1,8 +1,14 @@
 from __future__ import unicode_literals
 
+from frappe import _
+
 
 def get_data():
 	return {
 		"fieldname": "insurance_plan",
-		"transactions": [{"items": ["Item Insurance Eligibility", "Patient Insurance Policy"]}],
+		"transactions": [
+			{"label": _("Eligibility"), "items": ["Item Insurance Eligibility"]},
+			{"label": _("Insurance Policies"), "items": ["Patient Insurance Policy"]},
+			{"label": _("Transactions"), "items": ["Patient Insurance Coverage"]},
+		],
 	}
