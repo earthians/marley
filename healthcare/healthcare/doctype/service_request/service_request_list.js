9daf8587367cf49ea6b0f68dde98633884c6980e
Sajin S R <sajin@earthianslive.com>
2025-06-02 10:20:04 +0530

    fix(Service Request): update Service Request list to use explicit status conditions

--- healthcare/healthcare/doctype/service_request/service_request_list.js
+++ healthcare/healthcare/doctype/service_request/service_request_list.js
@@ -1,17 +1,24 @@
-frappe.listview_settings['Service Request'] = {
-	add_fields: ['name', 'status'],
-	filters: [['docstatus', '=', '1']],
+frappe.listview_settings["Service Request"] = {
+	add_fields: ["name", "status"],
+	filters: [["docstatus", "=", "1"]],
 	has_indicator_for_cancelled: 1,
 	get_indicator: function (doc) {
-		return [__(doc.status), {
-			'draft-Request Status': 'orange',
-			'active-Request Status': 'green',
-			'on-hold-Request Status': 'yellow',
-			'completed-Request Status': 'blue',
-			'revoked-Request Status': 'grey',
-			'replaced-Request Status': 'grey',
-			'unknown-Request Status': 'grey',
-			'entered-in-error-Request Status': 'red',
-		}[doc.status], 'status,=,' + doc.status];
+		if (doc.status == "draft-Request Status") {
+			return [__("Draft"), "orange", "status,=,Draft"];
+		} else if (doc.status == "active-Request Status") {
+			return [__("Active"), "green", "status,=,Active"];
+		} else if (doc.status == "on-hold-Request Status") {
+			return [__("On Hold"), "yellow", "status,=,On Hold"];
+		} else if (doc.status == "completed-Request Status") {
+			return [__("Completed"), "blue", "status,=,Completed"];
+		} else if (doc.status == "revoked-Request Status") {
+			return [__("Revoked"), "grey", "status,=,Revoked"];
+		} else if (doc.status == "replaced-Request Status") {
+			return [__("Replaced"), "grey", "status,=,Replaced"];
+		} else if (doc.status == "entered-in-error-Request Status") {
+			return [__("Entered In Error"), "red", "status,=,Entered In Error"];
+		} else {
+			return [__("Unknown"), "grey", "status,=,Unknown"];
+		}
 	}
 };
