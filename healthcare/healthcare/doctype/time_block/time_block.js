72b62531607d57a3f9589fb32da42e6132fabe66
Anoop Kurungadam <anoop@earthianslive.com>
2025-08-18 18:34:22 +0530

    chore: add doctype time block

---
+++ healthcare/healthcare/doctype/time_block/time_block.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2025, earthians Health Informatics Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Time Block", {
+//     refresh(frm) {
+//     }
+// });
---
+++ healthcare/healthcare/doctype/time_block/time_block.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, earthians Health Informatics Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class TimeBlock(Document):
+	pass
