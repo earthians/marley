a555324da0b3d88c3f5af8d1ce65432a4269423c
Anoop Kurungadam <anoop@earthianslive.com>
2025-09-01 20:26:44 +0530

    fix: typo

--- healthcare/healthcare/dashboard_chart_source/service_unit_type_wise_admission_status/service_unit_type_wise_admission_status.py
+++ healthcare/healthcare/dashboard_chart_source/service_unit_type_wise_admission_status/service_unit_type_wise_admission_status.py
@@ -4,8 +4,8 @@
 
 import frappe
 from frappe import _
-from frappe.utils.dashboard import cache_source
 from frappe.query_builder.functions import Count
+from frappe.utils.dashboard import cache_source
 
 
 @frappe.whitelist()
@@ -27,7 +27,11 @@ def get(
 		chart = frappe._dict(frappe.parse_json(chart))
 
 	filters = frappe.parse_json(filters)
-	company = filters.get("company") if filters and filters.get("company") else frappe.db.get_single_value("Global Defaults", "default_company")
+	company = (
+		filters.get("company")
+		if filters and filters.get("company")
+		else frappe.db.get_single_value("Global Defaults", "default_company")
+	)
 
 	ip = frappe.qb.DocType("Inpatient Record")
 
@@ -36,20 +40,22 @@ def get(
 		.select(
 			ip.admission_service_unit_type.as_("type"),
 			Count(ip.name).as_("total_ip_records"),
-			Count(frappe.qb.terms.Case().when(ip.status == "Admission Scheduled", 1)).as_("admission_scheduled"),
+			Count(frappe.qb.terms.Case().when(ip.status == "Admission Scheduled", 1)).as_(
+				"admission_scheduled"
+			),
+			Count(frappe.qb.terms.Case().when(ip.status == "Discharge Scheduled", 1)).as_(
+				"discharge_scheduled"
+			),
 			Count(frappe.qb.terms.Case().when(ip.status == "Admitted", 1)).as_("admitted"),
-			Count(frappe.qb.terms.Case().when(ip.status == "Discharge Scheduled", 1)).as_("discharge_scheduled"),
 			Count(frappe.qb.terms.Case().when(ip.status == "Discharged", 1)).as_("discharged"),
 		)
 		.where(
 			(ip.status != "Cancelled")
-			& (ip.admission_service_unit_type.isnotnull()
-			& (ip.company == company)
-		)
+			& (ip.admission_service_unit_type.isnotnull() & (ip.company == company))
 		)
 		.groupby(ip.admission_service_unit_type)
 	)
-	
+
 	data = query.run(as_dict=True)
 
 	labels = []
@@ -68,10 +74,10 @@ def get(
 	return {
 		"labels": labels,
 		"datasets": [
-			{"name": _("To Admission"), "values": admission_scheduled_ips},
 			{"name": _("Admitted"), "values": admitted_ips},
-			{"name": _("To Discharge"), "values": discharge_scheduled_ips},
 			{"name": _("Discharged"), "values": discharged_ips},
+			{"name": _("To Admit"), "values": admission_scheduled_ips},
+			{"name": _("To Discharge"), "values": discharge_scheduled_ips},
 		],
 		"type": "bar",
 	}
