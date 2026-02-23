420187afdf2c38d2e20d23d254d47d1b74358ac3
Anoop Kurungadam <anoop@earthianslive.com>
2026-02-23 11:59:04 +0530

    fix: remove unused code

--- healthcare/healthcare/doctype/patient_appointment/patient_appointment.js
+++ healthcare/healthcare/doctype/patient_appointment/patient_appointment.js
@@ -510,10 +510,6 @@ frappe.ui.form.on("Patient Appointment", {
 		}
 	},
 
-	get_procedure_from_encounter: function (frm) {
-		get_prescribed_procedure(frm);
-	},
-
 	toggle_payment_fields: function (frm) {
 		frappe.call({
 			method: "healthcare.healthcare.doctype.patient_appointment.patient_appointment.check_payment_reqd",
@@ -543,29 +539,6 @@ frappe.ui.form.on("Patient Appointment", {
 				}
 			},
 		});
-	},
-
-	get_prescribed_therapies: function (frm) {
-		if (frm.doc.patient) {
-			frappe.call({
-				method: "healthcare.healthcare.doctype.patient_appointment.patient_appointment.get_prescribed_therapies",
-				args: { patient: frm.doc.patient },
-				callback: function (r) {
-					if (r.message) {
-						show_therapy_types(frm, r.message);
-					} else {
-						frappe.msgprint({
-							title: __("Not Therapies Prescribed"),
-							message: __(
-								"There are no Therapies prescribed for Patient {0}",
-								[frm.doc.patient.bold()],
-							),
-							indicator: "blue",
-						});
-					}
-				},
-			});
-		}
 	},
 
 	get_orders: function (frm) {
@@ -1272,149 +1245,6 @@ let reschedule_dept_or_su = function (frm) {
 	});
 	d.set_value("appointment_date", frm.doc.appointment_date);
 	d.set_value(field_config.fieldname, frm.doc[field_config.fieldname]);
-	d.show();
-};
-
-let get_prescribed_procedure = function (frm) {
-	if (frm.doc.patient) {
-		frappe.call({
-			method: "healthcare.healthcare.doctype.patient_appointment.patient_appointment.get_procedure_prescribed",
-			args: { patient: frm.doc.patient },
-			callback: function (r) {
-				if (r.message && r.message.length) {
-					show_procedure_templates(frm, r.message);
-				} else {
-					frappe.msgprint({
-						title: __("Not Found"),
-						message: __(
-							"No Prescribed Procedures found for the selected Patient",
-						),
-					});
-				}
-			},
-		});
-	} else {
-		frappe.msgprint({
-			title: __("Not Allowed"),
-			message: __("Please select a Patient first"),
-		});
-	}
-};
-
-let show_procedure_templates = function (frm, result) {
-	let d = new frappe.ui.Dialog({
-		title: __("Prescribed Procedures"),
-		fields: [
-			{
-				fieldtype: "HTML",
-				fieldname: "procedure_template",
-			},
-		],
-	});
-	let html_field = d.fields_dict.procedure_template.$wrapper;
-	html_field.empty();
-	$.each(result, function (x, y) {
-		let row = $(
-			repl(
-				'<div class="col-xs-12" style="padding-top:12px; text-align:center; display:flex;" >\
-		<div class="col-xs-5"> %(encounter)s <br> %(consulting_practitioner)s <br> %(encounter_date)s </div>\
-		<div class="col-xs-5"> %(procedure_template)s <br>%(practitioner)s  <br> %(date)s</div>\
-		<div class="col-xs-2">\
-		<a data-name="%(name)s" data-procedure-template="%(procedure_template)s"\
-		data-encounter="%(encounter)s" data-practitioner="%(practitioner)s"\
-		data-date="%(date)s"  data-department="%(department)s">\
-		<button class="btn btn-default btn-xs">Add\
-		</button></a></div></div><hr/>',
-				{
-					name: y[0],
-					procedure_template: y[1],
-					encounter: y[2],
-					consulting_practitioner: y[3],
-					encounter_date: y[4],
-					practitioner: y[5] ? y[5] : "",
-					date: y[6] ? y[6] : "",
-					department: y[7] ? y[7] : "",
-				},
-			),
-		).appendTo(html_field);
-		row.find("a").click(function () {
-			frm.doc.procedure_template = $(this).attr("data-procedure-template");
-			frm.doc.procedure_prescription = $(this).attr("data-name");
-			frm.doc.practitioner = $(this).attr("data-practitioner");
-			frm.doc.appointment_date = $(this).attr("data-date");
-			frm.doc.department = $(this).attr("data-department");
-			refresh_field("procedure_template");
-			refresh_field("procedure_prescription");
-			refresh_field("appointment_date");
-			refresh_field("practitioner");
-			refresh_field("department");
-			d.hide();
-			return false;
-		});
-	});
-
-	d.show();
-};
-
-let show_therapy_types = function (frm, result) {
-	var d = new frappe.ui.Dialog({
-		title: __("Prescribed Therapies"),
-		fields: [
-			{
-				fieldtype: "HTML",
-				fieldname: "therapy_type",
-			},
-		],
-	});
-	var html_field = d.fields_dict.therapy_type.$wrapper;
-
-	$.each(result, function (x, y) {
-		var row = $(
-			repl(
-				'<div class="col-xs-12" style="padding-top:12px; text-align:center; display:flex;" >\
-		<div class="col-xs-5"> %(encounter)s <br> %(practitioner)s <br> %(date)s </div>\
-		<div class="col-xs-5"> %(therapy)s </div>\
-		<div class="col-xs-2">\
-		<a data-therapy="%(therapy)s" data-therapy-plan="%(therapy_plan)s" data-name="%(name)s"\
-		data-encounter="%(encounter)s" data-practitioner="%(practitioner)s"\
-		data-date="%(date)s"  data-department="%(department)s">\
-		<button class="btn btn-default btn-xs">Add\
-		</button></a></div></div><hr/>',
-				{
-					therapy: y[0],
-					name: y[1],
-					encounter: y[2],
-					practitioner: y[3],
-					date: y[4],
-					department: y[6] ? y[6] : "",
-					therapy_plan: y[5],
-				},
-			),
-		).appendTo(html_field);
-
-		row.find("a").click(function () {
-			frm.doc.therapy_type = $(this).attr("data-therapy");
-			frm.doc.practitioner = $(this).attr("data-practitioner");
-			frm.doc.department = $(this).attr("data-department");
-			frm.doc.therapy_plan = $(this).attr("data-therapy-plan");
-			frm.refresh_field("therapy_type");
-			frm.refresh_field("practitioner");
-			frm.refresh_field("department");
-			frm.refresh_field("therapy-plan");
-			frappe.db.get_value(
-				"Therapy Type",
-				frm.doc.therapy_type,
-				"default_duration",
-				r => {
-					if (r.default_duration) {
-						frm.set_value("duration", r.default_duration);
-					}
-				},
-			);
-			d.hide();
-			return false;
-		});
-	});
 	d.show();
 };
 
