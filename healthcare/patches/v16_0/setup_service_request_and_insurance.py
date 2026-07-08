e2d5e038c0f078c8ffed5b2dc0a553ec21f64d94
Anoop Kurungadam <anoop@earthianslive.com>
2026-07-08 13:26:07 +0530

    fix: call to removed function create_customer_groups

--- healthcare/patches/v16_0/setup_service_request_and_insurance.py
+++ healthcare/patches/v16_0/setup_service_request_and_insurance.py
@@ -2,14 +2,13 @@ import frappe
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
 from healthcare.healthcare.healthcare import data
-from healthcare.setup import create_customer_groups, setup_service_request_masters
+from healthcare.setup import setup_service_request_masters
 
 
 def execute():
 	frappe.reload_doc("healthcare", "doctype", "Patient Care Type")
 
 	setup_service_request_masters()
-	create_customer_groups()
 
 	frappe.reload_doc("accounts", "doctype", "sales_invoice")
 	frappe.reload_doc("accounts", "doctype", "sales_invoice_item")
