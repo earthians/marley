f92cbf2d89f2feae6e4858f97eb8e34b501a3f2e
Anoop Kurungadam <anoop@earthianslive.com>
2026-08-24 16:50:43 +0530

    feat: doctypes for patient i/o recording

---
+++ healthcare/healthcare/doctype/intake_output_type/intake_output_type.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2026, earthians Health Informatics Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+
+class IntakeOutputType(Document):
+	pass
---
+++ healthcare/patches/v16_0/create_intake_output_types.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2026, earthians Health Informatics Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+from healthcare.setup import create_intake_output_types
+
+
+def execute():
+	create_intake_output_types()
