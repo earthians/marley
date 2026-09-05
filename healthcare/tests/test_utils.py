06c5863377fa61b384b096cee8c392104b13d5b6
Anoop Kurungadam <anoop@earthianslive.com>
2026-09-05 20:12:39 +0530

    test(fee-validity): cover free follow ups on encounters
    (cherry picked from commit c7a3b050198054ea780e8c1e77c3f867e631d752)

--- healthcare/tests/test_utils.py
+++ healthcare/tests/test_utils.py
@@ -1,10 +1,13 @@
 import frappe
 
 
-def create_encounter(patient, practitioner, submit=False):
+def create_encounter(patient, practitioner, submit=False, **kwargs):
 	encounter = frappe.new_doc("Patient Encounter")
 	encounter.patient = patient
 	encounter.practitioner = practitioner
+	encounter.company = "_Test Company"
+	encounter.appointment_type = "_Test Appointment Type"
+	encounter.update(kwargs)
 	encounter.save()
 	if submit:
 		encounter.submit()
