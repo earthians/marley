17008318b96e265d47130ce822d639ad10c7f52a
Anoop Kurungadam <anoop@earthianslive.com>
2026-09-05 14:17:08 +0530

    feat: allergy master and patient allergy record
    (cherry picked from commit 4f7dd161330c7c35398097a8355fd9b5e68f3b72)

--- healthcare/healthcare/doctype/allergy/allergy.js
+++ healthcare/healthcare/doctype/allergy/allergy.js
@@ -1,7 +1,8 @@
 // Copyright (c) 2026, earthians Health Informatics Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("Patient Allergy", {
-	// refresh: function(frm) {
-	// }
+frappe.ui.form.on("Allergy", {
+	substance_type: function (frm) {
+		frm.set_value("substance", "");
+	},
 });
--- healthcare/healthcare/doctype/allergy/allergy.py
+++ healthcare/healthcare/doctype/allergy/allergy.py
@@ -1,10 +1,26 @@
-# Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and contributors
+# Copyright (c) 2026, earthians Health Informatics Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
 class Allergy(Document):
-	pass
+	def validate(self):
+		self.clear_substance_of_non_medication()
+		self.validate_substance()
+
+	def clear_substance_of_non_medication(self):
+		"""Only a medication allergen is checked against a prescription"""
+		if self.category == "Medication":
+			return
+
+		self.substance_type = None
+		self.substance = None
+
+	def validate_substance(self):
+		"""mandatory_depends_on is enforced in the browser only, and an allergen without a
+		substance would silently never match a prescription"""
+		if self.category == "Medication" and not self.substance:
+			frappe.throw(_("Substance is mandatory for a medication allergen"))
