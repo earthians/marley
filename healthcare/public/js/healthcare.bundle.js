f06b1ab0f72f96ea9415f6b150363ab45d0b82ac
Anoop Kurungadam <anoop@earthianslive.com>
2026-09-05 17:17:49 +0530

    feat: show medication alerts while prescribing
    (cherry picked from commit 5a7a4330bdea8fd729c2dec7518bb70c9e2d04f3)
    
    # Conflicts:
    #	healthcare/healthcare/doctype/patient_encounter/patient_encounter.js

--- healthcare/public/js/healthcare.bundle.js
+++ healthcare/public/js/healthcare.bundle.js
@@ -9,3 +9,4 @@ import "./healthcare_orders.html";
 import "./triage_indicator.js";
 import "./send_to_emergency.js";
 import "./sample_collection_tree.js";
+import "./medication_safety/medication_safety.js";
