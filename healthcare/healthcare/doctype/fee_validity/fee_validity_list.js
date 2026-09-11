8733ae37134c3eb46fcb6da125d184e208e8bace
Anoop Kurungadam <anoop@earthianslive.com>
2026-09-11 10:33:25 +0530

    fix(fee-validity): listview colours, filter
    (cherry picked from commit ff9624a17f4b232e664ebbc80efd73c667a81c6e)

--- healthcare/healthcare/doctype/fee_validity/fee_validity_list.js
+++ healthcare/healthcare/doctype/fee_validity/fee_validity_list.js
@@ -1,10 +1,11 @@
 frappe.listview_settings["Fee Validity"] = {
 	add_fields: ["practitioner", "patient", "status", "valid_till"],
+	filters: [["status", "=", "Active"]],
 	get_indicator: function (doc) {
 		const color = {
 			Active: "green",
-			Completed: "green",
-			Expired: "red",
+			Completed: "grey",
+			Expired: "orange",
 			Cancelled: "red",
 		};
 		return [__(doc.status), color[doc.status], "status,=," + doc.status];
