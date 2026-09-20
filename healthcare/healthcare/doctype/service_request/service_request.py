dda611beaf2f474ab66bdbacd3b194ed2460c4e6
Mohammed Umair <mohammedumair368@gmail.com>
2026-09-20 14:52:19 +0530

    fix: appointment creation from referral service request

--- healthcare/healthcare/doctype/service_request/service_request.py
+++ healthcare/healthcare/doctype/service_request/service_request.py
@@ -446,8 +446,10 @@ def make_appointment(
 		set_missing_values(source, target)
 
 	def set_missing_values(source, target):
-		target.department = frappe.db.get_value(
-			"Healthcare Practitioner", source.referred_to_practitioner, "department"
+		target.department, target.practitioner_name = frappe.db.get_value(
+			"Healthcare Practitioner",
+			source.referred_to_practitioner,
+			["department", "practitioner_name"],
 		)
 
 	doc = get_mapped_doc(
@@ -463,7 +465,7 @@ def make_appointment(
 					"source_doc": "reference_doctype",
 					"order_group": "reference_docname",
 				},
-				"field_no_map": ["naming_series", "status"],
+				"field_no_map": ["naming_series", "status", "source"],
 			},
 		},
 		target_doc,
